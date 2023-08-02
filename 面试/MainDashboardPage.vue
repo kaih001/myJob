e45530ada5ec934c56cd0ae6cd0be6256fc93e92
wangkaile <wangkaile@doumi.com>
2023-08-02 16:46:38 +0800

    更新面试题

--- 面试/MainDashboardPage.vue
+++ 面试/MainDashboardPage.vue
@@ -52,6 +52,14 @@
           面试官：与webpack类似的工具还有哪些？区别？
         </a>
       </li>
+      <li>
+        <a
+          href="https://developer.aliyun.com/article/1170842?spm=a2c6h.24874632.expert-profile.262.7f861b49fAgcZj"
+          target="_blank"
+        >
+          webpack5 新特性解析
+        </a>
+      </li>
     </ul>
     <h1>小程序</h1>
     <ul>
@@ -61,35 +69,253 @@
         </a>
       </li>
       <li>
-        <a href="https://developers.weixin.qq.com/miniprogram/dev/framework/performance/tips/start.html" target="_blank">
+        <a
+          href="https://developers.weixin.qq.com/miniprogram/dev/framework/performance/tips/start.html"
+          target="_blank"
+        >
           小程序性能优化有哪些 ？
         </a>
       </li>
       <li>
-        <a href="https://developers.weixin.qq.com/miniprogram/dev/framework/performance/tips/start.html" target="_blank">
+        <a
+          href="https://developers.weixin.qq.com/miniprogram/dev/framework/performance/tips/start.html"
+          target="_blank"
+        >
           小程序运行机制及更新机制？
         </a>
       </li>
       <li>
-        <a href="https://developers.weixin.qq.com/miniprogram/dev/framework/performance/tips/start.html" target="_blank">
-          小程序获取手机号信息有哪些  ？
+        <a
+          href="https://developers.weixin.qq.com/miniprogram/dev/framework/performance/tips/start.html"
+          target="_blank"
+        >
+          小程序获取手机号信息有哪些 ？
         </a>
       </li>
       <li></li>
     </ul>
-    <h1>手写题</h1>
+    <h1>网络</h1>
     <ul>
       <li>
-        <a href="" target="_blank">
-          手写一个字符逗号分隔函数 ？
+        <a href="https://zhuanlan.zhihu.com/p/133906695" target="_blank">
+          从输入 URL 到页面展示到底发生了什么？看完吊打面试官 ？
         </a>
       </li>
       <li>
-        <a href="https://mpxjs.cn/guide/basic/start.html" target="_blank">
-          为什么要用Mpx？Mpx是如何实现跨端的？
+        <a
+          href="https://mp.weixin.qq.com/s?__biz=MzAxODE2MjM1MA==&mid=2651553545&idx=2&sn=71efd68546b1c34485c0133053d4c693&chksm=8025a8c8b75221dedd749d7c3fab852850198384b8dfee2f57e3986cd27fdd674a6986e96d8b&mpshare=1&scene=24&srcid=022509Q6iuppXtkzjnbGzVBc&sharer_sharetime=1614215106928&sharer_shareid=b87408521fe1562a5a685dc8ddb08172#rd"
+          target="_blank"
+        >
+          浅谈浏览器 http 的缓存机制
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/6844903763665240072" target="_blank">
+          浏览器缓存
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/6844903763665240072" target="_blank">
+          浏览器渲染原理
         </a>
       </li>
     </ul>
+    <h1>Vu3</h1>
+    <ul>
+      <li>
+        <a
+          href="https://www.cnblogs.com/ygunoil/p/14463687.html"
+          target="_blank"
+        >
+          为什么说vue3要比vue2性能要好？
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/7227453567686033468" target="_blank"
+          >vue3面试题八股集合——2023
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/7109107369753182244" target="_blank">
+          Vue3 做了哪些优化
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/7107629309786865677" target="_blank">
+          揭开 Vue Router 的神秘面纱
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/7073119858107351048" target="_blank">
+          Vue 项目的权限管理
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/7054345738142089246" target="_blank">
+          Vue3中的新特性 Fragment、Suspense、Portal
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://juejin.cn/post/7089032094231298084#heading-22"
+          target="_blank"
+        >
+          Pinia进阶：再谈Pinia函数式（composition API）用法
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://juejin.cn/post/7204844328111374391#heading-14"
+          target="_blank"
+        >
+          2023前端vue面试题及答案
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/7116780561921736717" target="_blank">
+          细说Vue2的响应式原理
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/7209950095402598456" target="_blank">
+          滴滴前端一面常考vue面试题（持续更新中）
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://juejin.cn/post/6854573222231605256#heading-8"
+          target="_blank"
+        >
+          手写Vue-router核心原理，再也不怕面试官问我Vue-router原理
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://developer.aliyun.com/article/1263154#:~:text=vite%20%E6%98%AF%E5%9F%BA%E4%BA%8E%20es%20modules%20%E7%9A%84%EF%BC%8C%E5%9B%A0%E4%B8%BA%20vite%20%E4%B8%8D%E5%85%81%E8%AE%B8%20commonjs,vite%20%E5%B0%B1%E4%B8%8D%E4%BC%9A%E5%B0%86%E6%89%80%E6%9C%89%E7%9A%84%E4%BE%9D%E8%B5%96%E8%AF%BB%E5%8F%96%E4%B8%80%E9%81%8D%EF%BC%8C%E4%B9%9F%E5%B0%B1%E6%8F%90%E5%8D%87%E4%BA%86%E8%BF%90%E8%A1%8C%E6%95%88%E7%8E%87%EF%BC%9B%E8%80%8C%20webpack%20%E5%88%99%E6%9B%B4%E5%A4%9A%E7%9A%84%E5%85%B3%E6%B3%A8%E5%85%BC%E5%AE%B9%E6%80%A7%EF%BC%8C%20webpack%20%E5%9C%A8%E5%90%AF%E5%8A%A8%E7%9A%84%E6%97%B6%E5%80%99%E9%9C%80%E8%A6%81%E8%AF%BB%E5%8F%96%E6%89%80%E6%9C%89%E7%9A%84%E4%BE%9D%E8%B5%96%EF%BC%8C%E7%BB%9F%E4%B8%80%E6%A8%A1%E5%9D%97%E5%8C%96%E4%BB%A3%E7%A0%81%EF%BC%8C%E5%86%8D%E5%90%AF%E5%8A%A8%E6%9C%8D%E5%8A%A1%E5%99%A8%EF%BC%9B%E8%80%8C%20vite%20%E5%88%99%E6%98%AF%E7%9B%B4%E6%8E%A5%E5%90%AF%E5%8A%A8%E6%9C%8D%E5%8A%A1%E5%99%A8%E6%8C%89%E9%9C%80%E5%8A%A0%E8%BD%BD%EF%BC%8C%E6%9C%AA%E7%94%A8%E5%88%B0%E7%9A%84%E6%A8%A1%E5%9D%97%E4%B8%8D%E4%BC%9A%E5%8A%A0%E8%BD%BD%E3%80%82"
+          target="_blank"
+        >
+          vite相较于webpack的优势
+        </a>
+      </li>
+    </ul>
+    <h1>其他</h1>
+    <ul>
+      <li>
+        <a href="https://segmentfault.com/a/1190000041753539" target="_blank">
+          2022 前端性能优化最佳实践
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/7017009327705686029" target="_blank">
+          再战前端性能优化 超详细汇总
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/7014839145537208333" target="_blank">
+          前端性能优化之代码分割，bundle包减少了26%
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/7008072984858460196" target="_blank">
+          前端性能优化实战 从 30s 到 2s
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/7014427729189666824" target="_blank">
+          前端性能优化之图片优化，图片资源减少了57%
+        </a>
+      </li>
+      <li>
+        <a
+          href="https://juejin.cn/post/7261080561480089655?share_token=af2da25a-24bd-4bed-9dd8-662207663295"
+          target="_blank"
+        >
+          作为面试官，为什么我推荐组件库作为前端面试的亮点？
+        </a>
+      </li>
+      <li>
+        <a href="https://juejin.cn/post/7028086399601475591" target="_blank">
+          前端金额格式化处理
+        </a>
+      </li>
+    </ul>
+    <h1>冲刺</h1>
+    <ul>
+      <li>
+        <a href="https://segmentfault.com/a/1190000041753539" target="_blank">
+          前端性能优化有哪些？
+        </a>
+      </li>
+      <li>webpack配置优化有哪些？</li>
+      <li>Vue3 相关学习</li>
+    </ul>
+    <a-button type="success" @click="updateName('ll')">{{ count }}</a-button>
+    <a-button type="success" @click="useUpdateView">执行响应式</a-button>
+    <MyComponent class="atter" @click="handleMyComponent"></MyComponent>
+    <MyAsyncComponent></MyAsyncComponent>
   </a-card>
 </template>
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import {
+  computed,
+  onMounted,
+  reactive,
+  ref,
+  watchEffect,
+  defineAsyncComponent,
+  onBeforeMount,
+} from "vue";
+import MyComponent from "../component/MyComponent.vue";
+const MyAsyncComponent = defineAsyncComponent(
+  () => import("../component/MyAsyncComponent.vue")
+);
+import { useUpdateView } from "../hooks/useReactive";
+
+let count = ref(0);
+let effectCount = ref(0);
+
+const plusOne = computed({
+  get: () => count.value + 1,
+  set: (val) => {
+    count.value = val - 1;
+  },
+});
+
+const stopWatchEff = watchEffect(
+  () => {
+    console.log("mashangzixing", effectCount.value);
+  },
+  {
+    flush: "post",
+    onTrack(e) {
+      // debugger
+    },
+    onTrigger(e) {
+      // debugger
+    },
+  }
+);
+
+function updateName(names: string) {
+  console.log(plusOne.value);
+  plusOne.value++;
+  // stopWatchEff()
+  effectCount.value++;
+}
+function handleMyComponent() {
+  console.log("handleMyComponent");
+}
+
+console.log("父组件setup...");
+onBeforeMount(() => {
+  console.log("父组件onBeforeMount...");
+});
+onMounted(() => {
+  console.log("父组件onMounted...");
+});
+</script>
+<style>
+.atter {
+  color: black;
+}
+</style>
